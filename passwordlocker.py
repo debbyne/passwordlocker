934a9dab0fdbcf10d82fccf5143aaa366b7c1adb
debbyne <kinyadebbyne@gmail.com>
2022-02-19 21:41:21 +0300

    add user list

--- passwordlocker.py
+++ passwordlocker.py
@@ -3,10 +3,20 @@ class User:
     Class that creates the password locker account
     """
     user_list = []
-    def __init__(self,username,password):
+    def __init__(self,user_name,pass_word):
         '''
-           __init__ method that helps us define properties for our objects.
+           __init__ method that helps us define properties for our object.
+           user_name :name of user
+           user_password : password for a user
         '''
     
-        self.username = username
-        self.password = password
+        self.user_name = user_name
+        self.pass_word = pass_word
+
+    def save_user(self):
+         '''
+        this method adds a new user to the user list
+         '''   
+         User.user_list.append(self)  
+    @classmethod
+    def user_exist()    
