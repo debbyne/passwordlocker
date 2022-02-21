79c292a004b0a17176acf2cd2026207d5ea6dec6
debbyne <kinyadebbyne@gmail.com>
2022-02-21 07:46:58 +0300

    add functions to the main function

--- run.py
+++ run.py
@@ -6,9 +6,51 @@ def create_user_account(user_name , pass_word):
     '''
     new_user = User(user_name ,pass_word)
     return new_user
+def save_users(user):
+    '''
+    function thats saves a new user account
+    '''    
+    User.save_user()
+def check_existing_users(name):
+    '''
+    function that checks if a user account name already exists
+    '''
 
+    return User.user_exist(name)
+def display_user(cls):
+    '''
+    function that returns the user list
+    '''
+    return cls.user_list
+def log_in(user_name,pass_word):
+    '''
+    this function enables users to log into their acccounts
+     '''
+    log_in=User.log_in(user_name,pass_word)
+    if log_in!=False:
+        return User.log_in(user_name,pass_word)
+def create_credentials(Credential,account,credentialuser_name,credentialpass_word):   
 
-
+     '''
+     function that creates credentials
+     '''
+     new_credential = Credential(account,credentialuser_name, credentialpass_word)
+     return new_credential
+def save_credentials(credentials):
+    '''
+    function that addes a new credential to the credentials
+    '''
+    credentials.save_credentials()
+def delete_credential(Credential):
+    '''
+    function to delete a credential
+    ''' 
+    Credential.delete_credential 
+def display_credentials(Credential):
+    ''' 
+    function that displays existing credentials
+    '''
+    return Credential.credentials_list
 
 
 
